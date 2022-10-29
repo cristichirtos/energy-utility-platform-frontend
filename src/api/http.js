55b6c5c8102f3b31d5a5c4ad05c62fedbbd6e552
Cristian Chirtos <cristian.chirtos@wolfpack-digital.com>
2022-10-29 15:30:28 +0300

    Change axios call for debugging

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import store from "../app/store";
-export const BASE_URL = "localhost:3000";
+export const BASE_URL = "http://localhost:3000";
 export const HTTP = axios.create({
   baseURL: BASE_URL,
 });
--- src/api/services/auth.js
+++ src/api/services/auth.js
@@ -1,17 +1,24 @@
 import { BASE_URL, HTTP } from "../http";
 import store from "../../app/store";
+import axios from "axios"
 import { signIn, signOut } from "../../actions/auth";
 
 export async function AuthLogin(data) {
-  const response = await HTTP.post("/login", data);
-  if (response.data.access_token) {
-    // response.data.role = response.data.role.substring(
-    //   1,
-    //   response.data.role.length - 1
-    // );
-    store.dispatch(signIn(response.data));
-  }
-  return response.data.role;
+  // First trial
+  axios.create({headers: {"Access-Control-Allow-Origin": "*"}}).post("http://localhost:3000/login");
+  // Second trial
+  //axios.post("http://localhost:3000/login");
+  
+  // This is the call that will be done, once axios works properly
+  // const response = await HTTP.post("/login", data: data);
+  // if (response.data.access_token) {
+  //   // response.data.role = response.data.role.substring(
+  //   //   1,
+  //   //   response.data.role.length - 1
+  //   // );
+  //   store.dispatch(signIn(response.data));
+  // }
+  // return response.data.role;
 }
 
 export function AuthLogout() {
